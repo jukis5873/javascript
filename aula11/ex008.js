dc7295022dd3310cc9b705087d43697aa1c7a24e
jukis5873 <87501184+jukis5873@users.noreply.github.com>
2022-08-29 12:14:35 -0300

    jvsaula10 11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { //condição simples!
+    console.log(`Vecê ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!!!`)
